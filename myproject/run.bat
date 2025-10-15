c47886c1c3c098df58b9d6482964963963bec0d7
Rohit <imrohit488@gmail.com>
2025-10-16 05:11:54 +0530

    removed bat and sh

--- myproject/run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM DevLocate startup script for Windows
-setlocal enabledelayedexpansion
-set PYTHONPATH=%CD%
-python -m uvicorn main:app --reload --port 8000
-pause
--- myproject/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# DevLocate startup script for Linux/Mac
-export PYTHONPATH="$(pwd)"
-python -m uvicorn main:app --reload --port 8000
